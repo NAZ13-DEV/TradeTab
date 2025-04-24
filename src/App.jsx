909cdf56f0f2176f3bf2535008a07c78e8e550a2
NAZ13-DEV <fnwayalani@gmail.com>
2025-04-24 21:48:45 +0100

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import AOS from 'aos';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'aos/dist/aos.css';
+
 import Home from "./pages/Home";
 import Metrics from "./pages/Metrics";
 import Journal from "./pages/Journal";
@@ -13,40 +15,53 @@ import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import PageNotFound from "./pages/PageNotFound";
 import EmotionalEvaluation from "./pages/EmotionalEvaluation";
-import 'aos/dist/aos.css';
 import ScrollToTop from "./components/ScrollToTop";
 
-
-
 const App = () => {
-
   useEffect(() => {
+    // Initialize animations
     AOS.init({
-      duration: 800, // animation duration (ms)
-      once: true,    // whether animation should happen only once
+      duration: 800,
+      once: true,
     });
+
+    // Preload all <img> elements after a short delay
+    const preloadImages = () => {
+      const images = document.querySelectorAll("img");
+      images.forEach((img) => {
+        const src = img.getAttribute("src");
+        if (src) {
+          const image = new Image();
+          image.src = src;
+        }
+      });
+    };
+
+    // Delay slightly to ensure components are rendered
+    const timeout = setTimeout(preloadImages, 300);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
     <div>
       <BrowserRouter>
-      <ScrollToTop/>
-      <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/metrics" element={<Metrics/>} />
-        <Route path="/journal" element={<Journal/>} />
-        <Route path="/forexCalculator" element={<ForexCalculator/>} />
-        <Route path="/brokers" element={<Brokers/>} />
-        <Route path="/freeUniversity" element={<FreeUniversity/>} />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/forgotPassword" element={<ForgotPassword/>} />
-        <Route path="/emotionalEvaluation" element={<EmotionalEvaluation/>} />
-        <Route path="*" element={<PageNotFound/>} />
-      </Routes>
+        <ScrollToTop />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/metrics" element={<Metrics />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/forexCalculator" element={<ForexCalculator />} />
+          <Route path="/brokers" element={<Brokers />} />
+          <Route path="/freeUniversity" element={<FreeUniversity />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/emotionalEvaluation" element={<EmotionalEvaluation />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
       </BrowserRouter>
-     
     </div>
   );
 };
